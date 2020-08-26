3a9be9d25a3b0046dbaf2d850e04a3f2c070117d
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-08-26 23:36:32 +0530

    location permission setup done

--- app/src/main/java/com/example/mapsandlocation/MapsActivity.kt
+++ app/src/main/java/com/example/mapsandlocation/MapsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.mapsandlocation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,6 +14,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -23,6 +29,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
+    override fun onStart(){
+        requestFineLocation()
+        super.onStart()
+        when{
+            isLocationGranted() ->{
+                setUpLocationListener()
+            }else->{
+            requestFineLocation()
+        }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            777-> if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                setUpLocationListener()
+            }else{
+                Toast.makeText(this, "Permission not granted", LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setUpLocationListener() {
+
+    }
+
+    fun isLocationGranted(): Boolean {
+        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestFineLocation() {
+        this.requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            777
+        )
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -34,10 +82,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isZoomGesturesEnabled = true
+            isMyLocationButtonEnabled = true
+            isCompassEnabled = true
+        }
+        mMap.setMaxZoomPreference(14f)
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addPolyline(
+            PolylineOptions()
+                .add(sydney, LatLng(20.59,78.39))
+                .color(ContextCompat.getColor(baseContext, R.color.colorPrimary))
+        )
+            .width = 2f
+
     }
 }
